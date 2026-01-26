a028b99841b508b3385c6514b675dfbcd575abc9
Anudit Nagar <nagaranudit@gmail.com>
2026-01-26 19:40:41 +0530

    Fix Build

--- src/index.ts
+++ src/index.ts
@@ -6,35 +6,14 @@ const server = serve({
     // Serve index.html for all unmatched routes.
     "/*": index,
     "/tts.worker.js": async () => {
-          const build = await Bun.build({
-            entrypoints: ["./src/tts.worker.ts"],
-            target: "browser",
-            minify: process.env.NODE_ENV === "production",
-          });
-
-          // Return the compiled JS
-          return new Response(build.outputs[0]);
-        },
-    "/api/hello": {
-      async GET(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "GET",
-        });
-      },
-      async PUT(req) {
-        return Response.json({
-          message: "Hello, world!",
-          method: "PUT",
-        });
-      },
-    },
-
-    "/api/hello/:name": async req => {
-      const name = req.params.name;
-      return Response.json({
-        message: `Hello, ${name}!`,
+      const build = await Bun.build({
+        entrypoints: ["./src/tts.worker.ts"],
+        target: "browser",
+        minify: process.env.NODE_ENV === "production",
       });
+
+      // Return the compiled JS
+      return new Response(build.outputs[0]);
     },
   },
 
